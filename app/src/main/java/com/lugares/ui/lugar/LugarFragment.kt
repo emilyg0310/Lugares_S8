c17804a446efde75c18e9cac874738a83e95c336
wmoli <uso.docente.2022@gmail.com>
2022-02-12 10:40:23 -0600

    Semana 03

--- app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
@@ -4,18 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.lugares.R
 import com.lugares.databinding.FragmentLugarBinding
 import com.lugares.viewmodel.LugarViewModel
 
 class LugarFragment : Fragment() {
 
+    private lateinit var lugarViewModel: LugarViewModel
     private var _binding: FragmentLugarBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -23,13 +22,15 @@ class LugarFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val lugarViewModel =
-            ViewModelProvider(this).get(LugarViewModel::class.java)
+        lugarViewModel =ViewModelProvider(this).get(LugarViewModel::class.java)
 
         _binding = FragmentLugarBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        return root
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)
+        }
+
+        return binding.root
     }
 
     override fun onDestroyView() {
