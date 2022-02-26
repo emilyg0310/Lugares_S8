41d986f50101c50bc588b34fe335ee84af94e84e
wmoli <uso.docente.2022@gmail.com>
2022-02-26 10:38:26 -0600

    Semana 05

--- app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
@@ -1,13 +1,19 @@
 package com.lugares.ui.lugar
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.lugares.R
 import com.lugares.databinding.FragmentAddLugarBinding
 import com.lugares.model.Lugar
@@ -27,11 +33,47 @@ class AddLugarFragment : Fragment() {
 
         _binding = FragmentAddLugarBinding.inflate(inflater, container, false)
 
-        binding.btAddLugar.setOnClickListener {
-            agregarLugar()
-        }
+        binding.btAddLugar.setOnClickListener { agregarLugar()  }
+
+        ubicaGPS()
 
         return binding.root
+    }
+
+    private var conPermisos:Boolean=true
+
+    private fun ubicaGPS() {
+        //Ubicacion tendrá la info de las coordenadas y demás
+        val ubicacion: FusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+
+        //Se valida si se tienen los permisos
+        if (ActivityCompat.checkSelfPermission(requireContext(),Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(requireContext(),Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED) {
+
+            //Se solicitan los permisos al usuario
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION),105)
+        }
+
+        //Efectivamente se busca la info del gps
+        if(conPermisos) {
+            ubicacion.lastLocation.addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    binding.tvLatitud.text = "${location.latitude}"
+                    binding.tvLongitud.text = "${location.longitude}"
+                    binding.tvAltura.text = "${location.altitude}"
+                } else {
+                    binding.tvLatitud.text = "9.9358"
+                    binding.tvLongitud.text = "-84.0360"
+                    binding.tvAltura.text = "0.0"
+                }
+            }
+        }
+
     }
 
     private fun agregarLugar() {
@@ -40,8 +82,14 @@ class AddLugarFragment : Fragment() {
             val correo=binding.etCorreo.text.toString()
             val telefono=binding.etTelefono.text.toString()
             val web=binding.etWeb.text.toString()
-            val lugar= Lugar(0,nombre,correo,telefono,web,0.0,
-                0.0,0.0,"","")
+            val latitud = binding.tvLatitud.text.toString().toDouble()
+            val longitud = binding.tvLongitud.text.toString().toDouble()
+            val altura = binding.tvAltura.text.toString().toDouble()
+
+            val lugar= Lugar(0,nombre,correo,telefono,web,
+                latitud,longitud,altura,
+                "","")
+
             lugarViewModel.addLugar(lugar)
             Toast.makeText(requireContext(),
                 getString(R.string.msg_lugar_add),
--- app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
@@ -1,6 +1,10 @@
 package com.lugares.ui.lugar
 
+import android.Manifest
 import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -34,14 +38,112 @@ class UpdateLugarFragment : Fragment() {
         binding.etCorreo.setText(args.lugar.correo)
         binding.etTelefono.setText(args.lugar.telefono)
         binding.etWeb.setText(args.lugar.web)
+        binding.tvLatitud.text=args.lugar.latitud.toString()
+        binding.tvLongitud.text=args.lugar.longitud.toString()
+        binding.tvAltura.text=args.lugar.altura.toString()
 
-        binding.btUpdateLugar.setOnClickListener {
-            actualizarLugar()
-        }
+        binding.btUpdateLugar.setOnClickListener { actualizarLugar() }
+        binding.btEmail.setOnClickListener { enviarCorreo() }
+        binding.btPhone.setOnClickListener { hacerLlamada() }
+        binding.btWhatsapp.setOnClickListener { enviarWhatsApp() }
+        binding.btWeb.setOnClickListener { verWeb() }
+        binding.btLocation.setOnClickListener { verMapa() }
 
         setHasOptionsMenu(true)  //Este fragmento debe tener un menu adicional
 
         return binding.root
+    }
+
+    private fun enviarCorreo() {
+        val correo = binding.etCorreo.text.toString()  //Se extrae la cuenta de correo del lugar...
+        if (correo.isNotEmpty()) {   //Podemos usar el recurso...
+            val intent = Intent(Intent.ACTION_SEND)  //Se va a enviar algo desde el app
+            intent.type = "message/rfc822"  //Se va a enviar un correo electrónico
+
+            //Se define el destinatario
+            intent.putExtra(Intent.EXTRA_EMAIL,arrayOf(correo))
+
+            //Se define el asunto
+            intent.putExtra(Intent.EXTRA_SUBJECT,getString(R.string.msg_saludos)+" "+binding.etNombre.text)
+
+            //SE define el cuerpo del correo inicial
+            intent.putExtra(Intent.EXTRA_TEXT,getString(R.string.msg_mensaje_correo))
+
+            //Se solicita el recurso de correo para que se envie este
+            startActivity(intent)
+
+        } else {  //No podemos usar el recurso
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun hacerLlamada() {
+        val telefono = binding.etTelefono.text.toString()  //Se extrae el número de telefono del lugar...
+        if (telefono.isNotEmpty()) {   //Podemos usar el recurso...
+            val intent = Intent(Intent.ACTION_CALL)  //Se va a llamar desde el app
+            intent.data = Uri.parse("tel:$telefono")  //Con ese prefijo se sabe que es un número de telefono
+
+            //Se procede a validar si hay o no permisos otorgados para hacer la llamada
+            if (requireActivity().checkSelfPermission(Manifest.permission.CALL_PHONE) !=
+                    PackageManager.PERMISSION_GRANTED) {  //Si no tenemos los permisos.. hay que solicitarlos al usuario
+
+                requireActivity().requestPermissions(arrayOf(Manifest.permission.CALL_PHONE),105)
+
+            } else {   //Si se han otorgado los permisos... anteriormente
+                requireActivity().startActivity(intent)  //Se hace la llamada telefónica...
+            }
+
+        } else {  //No podemos usar el recurso
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun enviarWhatsApp() {
+        val telefono = binding.etTelefono.text.toString()  //Se extrae el número de telefono del lugar...
+        if (telefono.isNotEmpty()) {   //Podemos usar el recurso...
+            val intent = Intent(Intent.ACTION_VIEW)  //Se va a llamar desde el app
+            val uri="whatsapp://send?phone=506$telefono&text="+  //Con ese prefijo se sabe que es un número de telefono
+                    getString(R.string.msg_saludos)
+
+            //Se establece el App a usar
+            intent.setPackage("com.whatsapp")
+            intent.data = Uri.parse(uri)   //Se carga la info
+            startActivity(intent)  //
+
+        } else {  //No podemos usar el recurso
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    private fun verWeb() {
+        val sitio = binding.etWeb.text.toString()  //Se extrae el sitio web del lugar...
+        if (sitio.isNotEmpty()) {   //Podemos usar el recurso...
+            val web = Uri.parse("http://$sitio")
+            val intent = Intent(Intent.ACTION_VIEW,web)  //Se ve el sitio web desde el app
+
+            startActivity(intent)  // se habre el visor web y se muestra el sitio web
+
+        } else {  //No podemos usar el recurso
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    //Regresamos a las 9:50
+
+    private fun verMapa() {
+        val latitud = binding.tvLatitud.text.toString().toDouble()  //Se extrae la latitud y se pasa a double
+        val longitud = binding.tvLongitud.text.toString().toDouble()  //Se extrae la longitud y se pasa a double
+
+        if (latitud.isFinite() && longitud.isFinite()) {   //Podemos usar el recurso...
+            val location = Uri.parse("geo:$latitud,$longitud?z=18")
+            val intent = Intent(Intent.ACTION_VIEW,location)  //Se ve el mapa del lugar desde el app
+
+            startActivity(intent)  // se habre el visor de mapas y se ve el lugar...
+
+        } else {  //No podemos usar el recurso
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
